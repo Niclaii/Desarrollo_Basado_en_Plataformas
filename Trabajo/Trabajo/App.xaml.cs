08ea3564fe0c212d674e595e18fcaf50f7517fde
Nicolas Quintanilla <nicolas.quintanilla@ucsp.edu.pe>
2023-11-22 17:16:03 -0500

    Xamarin corregido
    Este si funciona

---
+++ Trabajo/Trabajo/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Trabajo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Trabajo/Trabajo/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Trabajo
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
